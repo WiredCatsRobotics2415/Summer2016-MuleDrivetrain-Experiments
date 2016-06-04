eb555e17db55f295dd6f114934cae032091aad6e
Arxos0012 <booker.oronde@gmail.com>
2016-06-04 17:09:04 -0400

    First Push: Basic Tank Drive
    when i say basic i mean hella basic no interpolation no deadband nothing
    zilch nada

---
+++ Mew/src/org/usfirst/frc/team2415/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team2415.robot.subsystems;
+
+import org.usfirst.frc.team2415.robot.RobotMap;
+import org.usfirst.frc.team2415.robot.commands.DriveCommand;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveSubsystem extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	private CANTalon leftTalon, rightTalon;
+	private Encoder leftEncoder, rightEncoder;
+	
+	public DriveSubsystem(){
+		leftTalon = new CANTalon(RobotMap.LEFT_TALON);
+		rightTalon = new CANTalon(RobotMap.RIGHT_TALON);
+		
+		leftEncoder = new Encoder(RobotMap.LEFT_ENCODER[0], RobotMap.LEFT_ENCODER[1]);
+		rightEncoder = new Encoder(RobotMap.RIGHT_ENCODER[0], RobotMap.RIGHT_ENCODER[1]);
+	}
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new DriveCommand());
+    }
+    
+    public void setMotors(double left, double right){
+    	leftTalon.set(left);
+    	rightTalon.set(right);
+    }
+    
+    public double[] getTalons(){
+    	return new double[]{leftTalon.get(), rightTalon.get()};
+    }
+    
+    public double[] getEncoders(){
+    	return new double[]{leftEncoder.get(), -rightEncoder.get()};
+    }
+    
+    public void resetEncoders(){
+    	leftEncoder.reset();
+    	rightEncoder.reset();
+    }
+    
+}
+
