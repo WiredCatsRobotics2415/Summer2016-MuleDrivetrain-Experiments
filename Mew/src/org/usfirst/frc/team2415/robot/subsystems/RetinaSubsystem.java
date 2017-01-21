745aae0c5de884d6b5b7e419874b4ff814a78ed7
omarimatthews <omarimatthews@10.40.16.103>
2017-01-21 14:29:27 -0500

    ...?

--- Mew/src/org/usfirst/frc/team2415/robot/subsystems/RetinaSubsystem.java
+++ Mew/src/org/usfirst/frc/team2415/robot/subsystems/RetinaSubsystem.java
@@ -16,7 +16,6 @@ public class RetinaSubsystem extends Subsystem {
     // here. Call these from Commands.
 	
 	public PixyCam pixyCam;
-	public AnalogInput pixy;
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
@@ -26,13 +25,9 @@ public class RetinaSubsystem extends Subsystem {
 	public RetinaSubsystem(){
     	pixyCam = new PixyCam(RobotMap.PIXY_PORT1,RobotMap.PIXY_PORT2);
     	pixyCam.setGoal(1.69);
-//		pixy = new AnalogInput(0);
 	}
     
     public void updateStatus(){
-//    	SmartDashboard.putNumber("Pixy Voltage", pixyCam.get());
-//    	SmartDashboard.putNumber("Smart Pixy Voltage", pixyCam.getErrorPrime());
-//    	SmartDashboard.putBoolean("Target Exist?", pixyCam.getTarget());
     }
 }
 
